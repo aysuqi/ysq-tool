7376a6086385cc08f53d350f825bc5e6dcd0b7fb
yusunqi <ysuqi01@126.com>
2023-09-16 23:44:27 +0800

    perf: 优化打包配置

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,17 @@
  * @Description:
  * @Author: yusunqi
  * @Date: 2023-09-16 14:18:59
- * @LastEditTime: 2023-09-16 22:26:56
+ * @LastEditTime: 2023-09-16 23:39:43
  * Copyright (c) 2023 by yusunqi, All Rights Reserved.
  */
-
-import { format } from './core'
+import * as core from './core'
 
 export * from './core'
-export default { format }
+
+const Ys = { format: core.format }
 
 if (typeof window === 'undefined') {
 	throw new Error('this is no window ENV')
 }
+
+export default Ys
